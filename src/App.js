407667c0a79d1467bede083ec376aef55aa216af
Dipesh Baral <dipeshbaral2014@gmail.com>
2024-12-08 11:13:07 +1100

    File Structure Changed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import LoginPage from "./components/LoginPage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
